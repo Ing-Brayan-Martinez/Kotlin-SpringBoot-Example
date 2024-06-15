ab063312742a9c3ddd805e9b2d9b563fd8213104
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-06-15 15:46:26 -0400

    optimizar servicios

--- .github/database/e-commerce.sql
+++ .github/database/e-commerce.sql
@@ -25,3 +25,15 @@ CREATE TABLE IF NOT EXISTS public."Product" (
   "brand_id" integer NOT NULL,
   "category_id" integer NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS public."Product" (
+  "id" serial NOT NULL,
+  "create_at" timestamp NOT NULL DEFAULT now(),
+  "update_at" timestamp NOT NULL DEFAULT now(),
+  "name" varchar NOT NULL,
+  "description" varchar NULL,
+  "price" decimal NULL,
+  "stock" integer NOT NULL,
+  "brand_id" integer NOT NULL,
+  "category_id" integer NOT NULL
+);
